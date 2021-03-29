856fb5f069b76687cc0c5f4d073680b12c352545
Stephan Sahm <stephan.sahm@gmx.de>
2021-03-29 11:16:16 +0200

    fixing typeinstance for either to also include Option and Try

--- src/TypeInstances_DataTypesBasic/Either.jl
+++ src/TypeInstances_DataTypesBasic/Either.jl
@@ -4,19 +4,21 @@
 # there is no neutral definition for Either, however both combine and orelse make sense
 # this also leverages sequence for dict and vector
 
-# orelse keeps first Right
-TypeClasses.orelse(x1::Identity, x2::Union{Const, Identity}) = x1
-TypeClasses.orelse(x1::Const, x2::Identity) = x2
+# orelse keeps the first Identity, and does not know about how to combine values in case of several identities
+TypeClasses.orelse(x1::Identity, x2::Union{Const, Identity}) = x1 
+TypeClasses.orelse(x1::Const, x2::Union{Const, Identity}) = x2
 
 TypeClasses.neutral(::Type{Either{L, R}}) where {L, R} = Identity(neutral(R))
-TypeClasses.neutral(::Type{Either{<:Any, R}}) where {R} = Identity(neutral(R))
+TypeClasses.neutral(::Type{Either{<:UL, R}}) where {UL, R} = Identity(neutral(R))
 TypeClasses.neutral(::Type{Option{T}}) where T = Const(nothing)
 TypeClasses.neutral(::Type{Option}) =  Const(nothing)
 
-# analog to definition of Option
-# combine keeps Right as Lefts could be regarded as neutral elements which must have no effect on combine by convention
+# `combine` is quite similar to `orelse`, with the key difference that it tries to combine the underlying values in case of two Identity
+# This semantic follows the idea of Const{Nothing} being an neutral element for values which just support [`combine`](@ref), but no `neutral`.
+TypeClasses.combine(x1::Identity, x2::Identity) = Identity(combine(x1.value, x2.value))
 TypeClasses.combine(x1::Identity, x2::Const) = x1
 TypeClasses.combine(x1::Const, x2::Identity) = x2
+TypeClasses.combine(x1::Const, x2::Const) = x2
 
 
 # FunctorApplicativeMonad
@@ -25,10 +27,10 @@ TypeClasses.combine(x1::Const, x2::Identity) = x2
 TypeClasses.ap(f::Const, x::Identity) = f
 TypeClasses.ap(f::Identity, x::Const) = x
 
-TypeClasses.pure(::Type{Either}, a) = Identity(a)
-TypeClasses.pure(::Type{Either{L, <:Any}}, a) where L = Identity(a)
-TypeClasses.pure(::Type{Either{<:Any, R}}, a) where R = Identity(a)
-TypeClasses.pure(::Type{Either{L, R}}, a) where {L, R} = Identity(a)
+TypeClasses.pure(::Type{Either{L, R}}, a) where {L, R} = Identity(a)  # includes Option, as Const{Nothing} => L=Nothing
+TypeClasses.pure(::Type{Either{L, <:UR}}, a) where {L, UR} = Identity(a)
+TypeClasses.pure(::Type{Either{<:UL, R}}, a) where {UL, R} = Identity(a)  # includes Try as Const{<:Exception} => UL = Exception
+TypeClasses.pure(::Type{Either{<:UL, <:UR}}, a) where {UL, UR} = Identity(a)
 
 
 # FlipTypes
